12824b7b4f972857e76ba5640c171f5a95e77432
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2023-10-12 19:51:47 +0200

    Fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,15 @@
 import "./Footer.scss"
+import * as React from 'react';
 import { useRef } from "react"
 
-export default function Footer({ handleAddTodo }: { handleAddTodo: (event: EventTarget) => void }) {
+export default function Footer({ handleAddTodo }: { handleAddTodo: (task: string) => void }) {
 
-    const refValue = useRef<string>('')
+    const ref = useRef<HTMLInputElement | null>(null)
     console.log()
     return (
         <footer>
-            <input type="text" placeholder="Add a new task" ref={refValue} />
-            <button onClick={() => handleAddTodo(refValue.current?.value)}><img src="add.svg" alt="add" /></button>
+            <input type="text" placeholder="Add a new task" ref={ref} />
+            <button onClick={() => handleAddTodo(ref.current?.value || '')}><img src="add.svg" alt="add" /></button>
         </footer>
     )
 }
